cb99857ba1f337bc60dc35cb1f3a010cdc980cd0
Xavi Torelló <info@xaviertorello.cat>
2017-12-15 13:08:34 +0100

    Define initial FACe instance spec

---
+++ specs/face_specs.py
@@ -0,0 +1,9 @@
+from face import FACe
+
+
+with description('A new'):
+    with before.each:
+        pass
+    with context('FACe instance'):
+        with it('must be initialized properly'):
+            pass
