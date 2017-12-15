58f822c2e73ca1222eac5f7e96f683ee0246ffe7
Xavi Torelló <info@xaviertorello.cat>
2017-12-15 13:01:18 +0100

    Create package face

---
+++ face/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import (division, absolute_import, print_function, unicode_literals)
+
+__author__ = 'XaviTorello'
+__version__ = '0.1.0'
+
+from .FACe import FACe
