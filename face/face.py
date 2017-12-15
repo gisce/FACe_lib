aea9485f27c78883b915518a2b97fcab1110c2de
Xavi Torelló <info@xaviertorello.cat>
2017-12-15 13:17:56 +0100

    Initialize a FACe instance with their zeep client and the signer plugin

--- face/face.py
+++ face/face.py
@@ -2,6 +2,19 @@
 from FACe_signer import FACe_signer
 import zeep
 
+# FACe environments
+FACE_ENVS = {
+    'prod': "https://webservice.face.gob.es/facturasspp2?wsdl",
+    'staging': "https://se-face-webservice.redsara.es/facturasspp2?wsdl",
+}
+
 class FACe(object):
-    def __init__(self):
-        pass
+    def __init__(self, **kwargs):
+        assert "certificate" in kwargs, "The certificate for requests signing must be defined"
+        self.certificate = kwargs['certificate']
+
+        # initialize a ZEEP client with the desired FACe envs
+        self.client = zeep.Client(
+            FACE_ENVS['staging'],
+            plugins=[FACe_signer(self.certificate, debug=True)]
+        )
