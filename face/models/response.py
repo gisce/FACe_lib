9955493b71ec357b1bed0fffe89a1b36d0db6197
Xavi Torelló <info@xaviertorello.cat>
2017-12-15 16:20:41 +0100

    Add FACe resultado schema

--- face/models/response.py
+++ face/models/response.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+from marshmallow import fields, Schema
+
+class ResultadoSchema(Schema):
+    codigo = fields.Integer()
+    descripcion = fields.String()
+    codigoSeguimiento = fields.String()
 
 class Response(object):
     pass
