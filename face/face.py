733ccc4045a659bb5d682c6d7a42c7e125582474
Xavi Torelló <info@xaviertorello.cat>
2017-12-15 13:44:26 +0100

    Handle environment at FACe init

--- face/face.py
+++ face/face.py
@@ -13,15 +13,23 @@ class FACe(object):
         assert "certificate" in kwargs and type(kwargs['certificate']) == str, "The certificate filename for requests signing must be defined"
         self.certificate = kwargs['certificate']
 
-        self.debug = True
-        # Handle optional kwarg
+        # Handle debug, df "False"
+        self.debug = False
         if 'debug' in kwargs:
             assert type(kwargs['debug']) == bool, "debug argument must be a boolean"
             self.debug = kwargs['debug']
 
+        # Handle environment, df "prod"
+        self.environment = "prod"
+        if 'environment' in kwargs:
+            assert type(kwargs['environment']) == str, "environment argument must be an string"
+            assert kwargs['environment'] in FACE_ENVS.keys(), "Provided environment '{}' not recognized in defined FACE_ENVS [{}]".format(kwargs['environment'], str(FACE_ENVS))
+            self.environment = kwargs['environment']
+
+
         # initialize a ZEEP client with the desired FACe envs
         self.client = zeep.Client(
-            FACE_ENVS['staging'],
+            FACE_ENVS[self.environment],
             plugins=[FACe_signer(self.certificate, debug=self.debug)]
         )
 
