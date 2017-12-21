c9436326325ab92c7b8eeb65ec3d9c197472e795
Xavi Torelló <info@xaviertorello.cat>
2017-12-21 18:02:09 +0100

    Create an Invoice SOAP_Service class to handle all invoce related reqs
    This will humanize the consumption of resources callin
    FACe.invoice.$method
    
    Invoice $methods:
    - send
    - cancel
    - list_states
    
    Fix #3

--- face/face.py
+++ face/face.py
@@ -19,6 +19,62 @@ class SOAP_service(object):
     def serialize_response(self, response):
         return serialize_object(response)
 
+
+class Invoice(SOAP_service):
+    """
+    Integrate all invoice-related methods
+    """
+    def send(self, invoice):
+        """
+        Send an invoice and return the delivery result
+
+        It prepares the payload wanted for the `enviarFactura` webservice with a base64 invoice and their filename
+        """
+        assert type(invoice) == str, "Invoice must the the filename of the invoice to deliver"
+        the_invoice = {
+            "correo": "devel@gisce.net",
+            "factura": {
+                "factura": base64.b64encode(open(invoice).read()),
+                "nombre": os.path.basename(invoice),
+                "mime": "application/xml",
+            }
+        }
+        call_result = self.serialize_response(self.service.enviarFactura(the_invoice))
+        schema = InvoiceSchema()
+        return schema.load(call_result)
+
+    def cancel(self, invoice, reason):
+        """
+        Cancel an invoice and return the result
+
+        It uses the invoice registry number (the one reached at the send process) and the cancelation reason.
+        """
+        assert type(invoice) == str, "Invoice registry number must be an string"
+        assert type(reason) == str, "The reason must be an string"
+
+        the_invoice = {
+            "numeroRegistro": invoice,
+            "motivo": reason,
+        }
+
+        call_result = serialize_object(self.service.anularFactura(**the_invoice))
+        schema = InvoiceSchema()
+        return schema.load(call_result)
+
+    def list_states(self):
+        """
+        List invoice states
+
+        It list all available invoice states. There are two flows:
+        - ordinary flow: it describes the lifecycle of the invoice
+        - cancelation flow: it describes the cancelation / anulation flow
+        """
+
+        call_result = serialize_object(self.service.consultarEstados())
+
+        schema = StatusesSchema()
+        return schema.load(call_result)
+
 class FACe(object):
     """
     FACe object
@@ -52,8 +108,8 @@ class FACe(object):
             plugins=[FACe_signer(self.certificate, debug=self.debug)]
         )
 
-    def serialize_response(self, response):
-        return serialize_object(response)
+        # Initialitze an invoice
+        self.invoice = Invoice(service=self.client.service)
 
     def list_nifs(self):
         """
@@ -66,42 +122,6 @@ class FACe(object):
         schema = ResponseSchema()
         return schema.load(call_result)
 
-    def send_invoice(self, invoice):
-        """
-        Send an invoice and return the delivery result
-
-        It prepares the payload wanted for the `enviarFactura` webservice with a base64 invoice and their filename
-        """
-        assert type(invoice) == str, "Invoice must the the filename of the invoice to deliver"
-        the_invoice = {
-            "correo": "devel@gisce.net",
-            "factura": {
-                "factura": base64.b64encode(open(invoice).read()),
-                "nombre": os.path.basename(invoice),
-                "mime": "application/xml",
-            }
-        }
-        call_result = self.serialize_response(self.client.service.enviarFactura(the_invoice))
-        schema = InvoiceSchema()
-        return schema.load(call_result)
-
-    def cancel_invoice(self, invoice, reason):
-        """
-        Cancel an invoice and return the result
-
-        It uses the invoice registry number (the one reached at the send process) and the cancelation reason.
-        """
-        assert type(invoice) == str, "Invoice registry number must be an string"
-        assert type(reason) == str, "The reason must be an string"
-
-        the_invoice = {
-            "numeroRegistro": invoice,
-            "motivo": reason,
-        }
-
-        call_result = serialize_object(self.client.service.anularFactura(**the_invoice))
-        schema = InvoiceSchema()
-        return schema.load(call_result)
 
     def list_administrations(self):
         """
@@ -113,18 +133,4 @@ class FACe(object):
         call_result = serialize_object(self.client.service.consultarAdministraciones())
 
         schema = AdministrationsSchema()
-        return schema.load(call_result)
-
-    def list_invoice_states(self):
-        """
-        List invoice states
-
-        It list all available invoice states. There are two flows:
-        - ordinary flow: it describes the lifecycle of the invoice
-        - cancelation flow: it describes the cancelation / anulation flow
-        """
-
-        call_result = serialize_object(self.client.service.consultarEstados())
-
-        schema = StatusesSchema()
         return schema.load(call_result)
