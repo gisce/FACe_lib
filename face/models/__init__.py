a9433b632a0b3b078812894639d13953748a1f4f
Xavi Torelló <info@xaviertorello.cat>
2017-12-21 11:41:45 +0100

    Add Statuses to models package

--- face/models/__init__.py
+++ face/models/__init__.py
@@ -2,3 +2,4 @@
 from .response import Response, ResponseSchema, ResultSchema, Result
 from .invoice import Invoice, InvoiceSchema
 from .administrations import Administrations, AdministrationsSchema
+from .status import Statuses, StatusesSchema
