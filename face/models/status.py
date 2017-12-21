7c721be6b4b73306f3f6363b9b307c31d30d173c
Xavi Torelló <info@xaviertorello.cat>
2017-12-21 15:45:22 +0100

    Add STATUS_CODES identified from FACe's manual

--- face/models/status.py
+++ face/models/status.py
@@ -3,6 +3,51 @@ from marshmallow import fields, Schema, post_load
 
 from .response import Response, ResponseSchema
 
+
+STATUS_CODES = {
+    # "Tramitación" status
+    "1200": {
+        "nombre": "Registrada",
+        "description": "La factura ha sido registrada en el registro electrónico REC",
+        "error": False,
+        "scope": "tramitacion",
+    },
+    "1300": {
+        "nombre": "Registrada en RCF",
+        "description": "La factura ha sido registrada en el RCF",
+        "error": False,
+        "scope": "tramitacion",
+    },
+    "2400": {
+        "nombre": "Contabilizada la obligación de pago",
+        "description": "La factura ha sido reconocida con obligación de pago",
+        "error": False,
+        "scope": "tramitacion",
+    },
+    "2500": {
+        "nombre": "Pagada",
+        "description": "Factura pagada",
+        "error": False,
+        "scope": "tramitacion",
+    },
+    "2600": {
+        "nombre": "Rechazada",
+        "description": "La Unidad rechaza la factura",
+        "error": True,
+        "scope": "tramitacion",
+    },
+    "3100": {
+        "nombre": "Anulada",
+        "description": "La Unidad aprueba la propuesta de anulación",
+        "error": False,
+        "scope": "tramitacion",
+    },
+
+}
+
+
+
+
 """
 FACe Status models and schemas
 """
